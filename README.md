c0025b5d4c57d1d8a859c7bc7d0a63be742a8f51
achraf charif <achraf.charif@cpexterne.org>
2023-01-16 21:54:22 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # messageBrokerStarter
+docker run --rm -it --hostname my-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
---
+++ src/test/java/com/rabbitmq/rabbitmqstarter/RabbitmqstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbitmqstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
